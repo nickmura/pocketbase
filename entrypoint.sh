190f4063d24a7df067bf0222bc29a3b04467a452
nickmura <dev@nickmura.ca>
2026-05-04 22:36:36 -0600

    updated entrypoint.sh to deploy hooks / migrations

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ set -e
 : "${DATA_DIR:=/data}"
 mkdir -p "$DATA_DIR"
 chown -R pocketbase:pocketbase "$DATA_DIR"
-exec su-exec pocketbase /usr/local/bin/pocketbase serve --http=0.0.0.0:${PORT} --dir="${DATA_DIR}"
+exec su-exec pocketbase /usr/local/bin/pocketbase serve --http=0.0.0.0:${PORT} --dir="${DATA_DIR}" --hooksDir=/app/pb_hooks --migrationsDir=/app/pb_migrations
